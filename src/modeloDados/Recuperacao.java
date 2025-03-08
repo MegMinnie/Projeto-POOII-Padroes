64143852884e0a7349c937ef000679d261cdd8c7
Mirele Oliveira <138227865+MegMinnie@users.noreply.github.com>
2025-03-08 12:12:03 -0300

    Update Recuperacao.java

--- src/modeloDados/Recuperacao.java
+++ src/modeloDados/Recuperacao.java
@@ -2,4 +2,7 @@ package src.modeloDados;
 
 public class Recuperacao implements IOEstados{
   
+   public void alterarNota( Nota nota, AlunoTurma alunoTurma){
+   alunoTurma.getNota().modificarNota(valor);
+   System.out.println("Nota alterada com sucesso!");
 }
