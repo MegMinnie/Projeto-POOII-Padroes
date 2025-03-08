1ea1268fdbeb452141e7049fe7562d17b7831a69
Mirele Oliveira <138227865+MegMinnie@users.noreply.github.com>
2025-03-08 12:06:11 -0300

    Update Ativo.java

--- src/modeloDados/Ativo.java
+++ src/modeloDados/Ativo.java
@@ -3,6 +3,8 @@ package src.modeloDados;
 public class Ativo implements IOEstados{
 
    public void add.Nota( Nota nota, AlunoTurma alunoTurma){
+   alunoTurma.getNota().adicionarNota(valor);
+   System.out.println("Nota adicionada com sucesso!");
 
 }
 }
