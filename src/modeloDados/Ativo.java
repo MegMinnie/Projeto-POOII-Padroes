ac60c7d2ccba0932f1e12464695c515489ad0719
Mirele Oliveira <138227865+MegMinnie@users.noreply.github.com>
2025-03-08 11:34:26 -0300

    Update Ativo.java

--- src/modeloDados/Ativo.java
+++ src/modeloDados/Ativo.java
@@ -1,5 +1,18 @@
 package src.modeloDados;
 
 public class Ativo implements IOEstados{
+
+private Estado estado;
+
+public Ativo( Estado estado){
+  this.estado = estado;
+}
+
+  public void atribuirNota(Nota nota, Estado estado, Professor professor){
+ if(professor.equals("professor")){
+   System.out.println("Apenas o professor pode atribuir nota ao aluno");
+   return;
+  }else{
+   aluno.setEstado( A
   
 }
