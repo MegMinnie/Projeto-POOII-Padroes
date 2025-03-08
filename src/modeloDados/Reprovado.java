13ab9c8551063dd95b44709db92f2bb9bd4b3601
Mirele Oliveira <138227865+MegMinnie@users.noreply.github.com>
2025-03-08 12:14:41 -0300

    Update Reprovado.java

--- src/modeloDados/Reprovado.java
+++ src/modeloDados/Reprovado.java
@@ -1,5 +1,7 @@
 package src.modeloDados;
 
 public class Repovado implements IOEstados{
-  
+   public void addNoa( Nota nota, AlunoTurma alunoTurma){
+   alunoTurma.getNota().modificarNota(valor);
+   System.out.println("Não é possível adicionar nota!");
 }
